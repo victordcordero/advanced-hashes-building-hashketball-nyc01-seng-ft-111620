afe5788b26532a6a05d9223bd21f7d43db63ced0
Victor Cordero <victordcordero@gmail.com>
2020-11-13 14:40:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -72,10 +72,10 @@ def game_hash
           :shoe => 18,
           :points => 10,
           :rebounds => 1,
-          :assists => 12,
-          :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 1
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2
         },
         {
         :player_names => "Reggie Evans",
