733d88ec4e757e08a3911f5c00e3361cd22a687c
Victor Cordero <victordcordero@gmail.com>
2020-11-13 14:40:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -78,15 +78,15 @@ def game_hash
           :slam_dunks => 2
         },
         {
-        :player_names => "Reggie Evans",
-        :number => 30,
-        :shoe => 14,
+        :player_names => "Bismack Biyombo",
+        :number => 0,
+        :shoe => 16,
         :points => 12,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 12,
-        :blocks => 12,
-        :slam_dunks => 7
+        :rebounds => 4,
+        :assists => 7,
+        :steals => 22,
+        :blocks => 15,
+        :slam_dunks => 10
       },
       {
       :player_names => "Brook Lopez",
