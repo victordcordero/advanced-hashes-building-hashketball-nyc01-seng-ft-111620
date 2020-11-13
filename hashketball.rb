fc7afec2ddfbea975489dcd9b73f3a89a8a47250
Victor Cordero <victordcordero@gmail.com>
2020-11-13 14:50:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -61,7 +61,8 @@ def game_hash
 :slam_dunks => 1
 },
         ]
-      },
+      }
+    {
       :away => {
           :team_name => "Charlotte Hornets",
           :colors => ["Turquoise, Purple"],
