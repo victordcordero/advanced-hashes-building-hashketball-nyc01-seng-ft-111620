9b458c5ee9183c118b005a6812281a1a9a5a4b57
Victor Cordero <victordcordero@gmail.com>
2020-11-13 14:35:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -62,5 +62,64 @@ def game_hash
 },
         ]
       }
+      :away => {
+        :team_name => "Brooklyn Nets",
+        :colors => ["Black, White"],
+        :players => [
+          {
+          :player_names => "Alan Anderson",
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounts => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
+        },
+        {
+        :player_names => "Reggie Evans",
+        :number => 30,
+        :shoe => 14,
+        :points => 12,
+        :rebounts => 12,
+        :assists => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7
+      },
+      {
+      :player_names => "Brook Lopez",
+      :number => 11,
+      :shoe => 17,
+      :points => 17,
+      :rebounts => 19,
+      :assists => 10,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 5
+    },
+    {
+    :player_names => "Mason Plumlee",
+    :number => 1,
+    :shoe => 19,
+    :points => 26,
+    :rebounts => 11,
+    :assists => 6,
+    :steals => 3,
+    :blocks => 8,
+    :slam_dunks => 5
+  },
+  {
+  :player_names => "Jason Terry",
+  :number => 31,
+  :shoe => 15,
+  :points => 19,
+  :rebounts => 2,
+  :assists => 2,
+  :steals => 4,
+  :blocks => 11,
+  :slam_dunks => 1
+  },
     }
 end
